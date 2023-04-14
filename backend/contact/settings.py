c54e031868829c1547beeba0b7c64dba47cfce01
Rhino <rhino@localhost.localdomain>
2023-04-14 19:35:41 +0530

    postgres added

--- backend/contact/settings.py
+++ backend/contact/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'contact.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'contact_form',
+        'USER': 'postgres',
+        'PASSWORD': 'Lalit2122',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
+
 
 
 # Password validation
